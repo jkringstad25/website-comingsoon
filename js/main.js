ccc2dd6cb01c28989925e05d4e4f05de2a54bf4f
jkringstad25 <justin.kringstad.2010@gmail.com>
2025-10-15 11:28:45 -0500

    first upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+
+            // FEATURES SECTION CAROUSEL
+            var carousel = document.getElementById("coming-soon-carousel");
+            var progressBar = document.querySelector(".carousel-progress .progress-bar");
+            var interval = 8000; // Change this to match data-bs-interval
+
+            function startProgressBar() {
+                progressBar.style.transition = "none";
+                progressBar.style.width = "0%";
+                setTimeout(() => {
+                    progressBar.style.transition = `width ${interval}ms linear`;
+                    progressBar.style.width = "100%";
+                }, 10);
+            }
+
+            var myCarousel = new bootstrap.Carousel(carousel, {
+                interval: interval,
+                ride: "carousel",
+                pause: "false",
+            });
+
+            carousel.addEventListener("slide.bs.carousel", function () {
+                startProgressBar();
+            });
+
+            startProgressBar(); // Start progress bar initially
+});
